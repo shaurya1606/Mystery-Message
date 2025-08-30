7ca2ab3b0407c88273833fa41a78e7e6a540bb66
Shaurya Srivastava <shaurya1606@users.noreply.github.com>
2025-08-30 22:35:46 +0530

    fixies bug

--- my-app/src/lib/dbConnect.ts
+++ my-app/src/lib/dbConnect.ts
@@ -20,8 +20,7 @@ async function dbConnect() : Promise<void>{
         console.log("Database connection status:", connection.isConnected);
     } catch (error) {
         console.error("Error connecting to database:", error);
-
-        process.exit(1);
+        throw error;
     }
 }
 
