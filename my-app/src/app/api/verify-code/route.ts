18eee4015308eb41c23352de6cce4f01935875ef
Shaurya Srivastava <shaurya1606@users.noreply.github.com>
2025-09-02 15:28:16 +0530

    Validation of verification code

---
+++ my-app/src/app/api/verify-code/route.ts
@@ -0,0 +1,77 @@
+import dbConnect from '@/lib/dbConnect';
+import UserModel from "@/model/User";
+
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+        const {username, code} = await request.json();
+
+        const decodedUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({username: decodedUsername});
+        if(!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 500
+                }
+            )
+        }
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if( isCodeValid && isCodeNotExpired) {
+            user.isVerified = true
+            await user.save();
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "User verified successfully"
+                },
+                {
+                    status: 200
+                }
+            );
+        }
+        if(!isCodeValid) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Invalid verification code"
+                },
+                {
+                    status: 400
+                }
+            );
+        }
+        if(!isCodeNotExpired) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Verification code expired"
+                },
+                {
+                    status: 400
+                }
+            );
+        } else {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Incorrect Verification code"
+                },
+                {
+                    status: 400
+                }
+            );
+        }
+
+    } catch (error) {
+
+    }
+}
