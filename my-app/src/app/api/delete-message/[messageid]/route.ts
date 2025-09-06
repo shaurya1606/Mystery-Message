c56c4719d4312ee2f82897686536a5e79966c4fd
Shaurya Srivastava <shaurya1606@users.noreply.github.com>
2025-09-07 00:10:32 +0530

    delete message api creation

---
+++ my-app/src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,59 @@
+import { auth } from "@/auth";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+export async function GET(request: Request, { params }: { params: { messageid: string } }) {
+    const messageId = params.messageid;
+    await dbConnect();
+
+    const session = await auth();
+    const user: User = session?.user as User;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "Unauthorized"
+            },
+            {
+                status: 401
+            }
+        )
+    }
+
+    try {
+        const updateResult =await UserModel.updateOne(
+            {_id: user._id},
+            {$pull: {messages:{_id: messageId}}}
+        )
+
+        if(updateResult.modifiedCount === 0){
+            return Response.json(
+                {
+                    success: false,
+                    message: "Message not found"
+                },
+                { status: 404 }
+            )
+        }
+        return Response.json(
+            {
+                success: true,
+                message: "Message deleted successfully"
+            },
+            { status: 200 }
+        );
+    }
+    catch (error) {
+        console.error("Error deleting message:", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error deleting message"
+            },
+            { status: 500 }
+        );
+    }
+        
+}
